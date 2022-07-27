fa96a852daa28d1968b24de3aa46cb72f4d65ba4
Diaa-E <blarb21@yahoo.com>
2022-07-27 17:20:18 +0200

    Add drawing-related functions

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const gridContainer = document.querySelector(".grid-container")
 
-function createGrid(dimensions = 16, gridContainer)
+function createGrid(gridContainer, dimensions = 16)
 {
     //change grid template based on new dimensions
     gridContainer.setAttribute('style', `grid-template-columns: repeat(${dimensions}, 1fr)`);
@@ -13,6 +13,20 @@ function createGrid(dimensions = 16, gridContainer)
         newCell.setAttribute('class', 'cell');
         gridContainer.appendChild(newCell);
     }
+
+    addCellListeners(document.querySelectorAll('.cell'));
 }
 
-createGrid(20, gridContainer);
+function addCellListeners(cells)
+{
+    cells.forEach(cell => cell.addEventListener('mouseover', (e) => {
+        fillCell(e.target);
+    }))
+}
+
+function fillCell(cell)
+{
+    cell.classList.add('cell-fill')
+}
+
+createGrid(gridContainer, 50);
